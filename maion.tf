aba0a228007e93657c5751db557e8e1d1e57df24
SuraJ7399 <95271793+SuraJ7399@users.noreply.github.com>
2021-12-07 14:42:03 +0530

    2.1

--- maion.tf
+++ maion.tf
@@ -10,15 +10,11 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
-  region  = "us-west-2"
+  profile = "Suraj"
+  region  = "ap-northeast-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0218d08a1f9dac831"
   instance_type = "t2.micro"
-
-  tags = {
-    Name = "ExampleAppServerInstance"
-  }
 }
