bfe81cafef7e304a8d425a5f09444871b6409be5
SuraJ7399 <95271793+SuraJ7399@users.noreply.github.com>
2021-12-07 14:26:35 +0530

    1.4

--- maion.tf
+++ maion.tf
@@ -1,7 +1,5 @@
 provider "aws" { 
-  access_key = "AKIASIP36E5FHIZE7FCB" 
-  secret_key = "6VHENbWpo6a8LKAnVwSiORpsO8Es9ut8IzeU59Dq" 
-  region = "us-east-1"}
+  region = "ap-northeast-1"}
 
 resource "aws_instance" "example" { 
   ami = "ami-0218d08a1f9dac831" 
