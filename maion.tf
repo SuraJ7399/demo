8f5bbebd5a75ea47bbfb48f9a4877af3f4388733
SuraJ7399 <95271793+SuraJ7399@users.noreply.github.com>
2021-12-07 14:39:54 +0530

    changing ec2 file 2.0

--- maion.tf
+++ maion.tf
@@ -1,9 +1,24 @@
-provider "aws" { 
-   access_key = "AKIASIP36E5FHIZE7FCB"
-  secret_key = "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"
-  region = "ap-northeast-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
 }
-resource "aws_instance" "example" { 
-  ami = "ami-0218d08a1f9dac831" 
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
