ac8c7b63d9b66db6956fd819bc33eb3bd6b9086f
SuraJ7399 <95271793+SuraJ7399@users.noreply.github.com>
2021-12-07 15:04:16 +0530

    3.0

--- maion.tf
+++ maion.tf
@@ -1,20 +1,8 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-
-  required_version = ">= 0.14.9"
-}
-
 provider "aws" {
-  profile = "Suraj"
-  region  = "ap-northeast-1"
+  region     = "ap-northeast-1"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "example" {
   ami           = "ami-0218d08a1f9dac831"
   instance_type = "t2.micro"
 }
