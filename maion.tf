5b8496b156871f542324c2f0028aac860e62e755
SuraJ7399 <95271793+SuraJ7399@users.noreply.github.com>
2021-12-08 15:39:39 +0530

    original form

--- maion.tf
+++ maion.tf
@@ -6,11 +6,4 @@ resource "aws_instance" "example" {
   ami           = "ami-0218d08a1f9dac831"
   instance_type = "t2.micro"
 }
-resource "aws_autoscaling_attachment" "asg_attachment_bar" {
-  autoscaling_group_name = aws_autoscaling_group.asg.id
-  elb                    = aws_elb.bar.id
-}
-resource "aws_autoscaling_attachment" "asg_attachment_bar" {
-  autoscaling_group_name = aws_autoscaling_group.asg.id
-  alb_target_group_arn   = aws_lb_target_group.test.arn
-}
+
